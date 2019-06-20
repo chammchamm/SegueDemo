d4c619c538ccef9c76dac95655d02f97ccb266ed
Jennifer Lin <chammy1111@gmail.com>
2019-06-20 16:03:58 +0800

    First version on June 20th, 2019

---
+++ SegueDemo/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SegueDemo
+//
+//  Created by Jennifer Lin on 2019/6/20.
+//  Copyright © 2019 Jennifer Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var btnShowOne: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
