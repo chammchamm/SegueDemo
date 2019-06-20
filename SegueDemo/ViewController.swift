b7455bf040d2f46e7a63ccb570f1603e63938fc1
Jennifer Lin <chammy1111@gmail.com>
2019-06-20 20:51:42 +0800

    commit on June 20th, 2019

--- SegueDemo/ViewController.swift
+++ SegueDemo/ViewController.swift
@@ -9,13 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var btnShowOne: UIButton!
+    @IBOutlet weak var btnShowTwo: UIButton!
+    
+    @IBAction func btnShowOneClicked(_ sender: Any) {
+    self.performSegue(withIdentifier: "mySegue", sender: btnShowOne)
+    }
+
+    @IBAction func btnShowTwoClicked(_ sender: Any) {
+    self.performSegue(withIdentifier: "mySegue", sender: btnShowTwo)
+    }
+
+//    self.performSegue(
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("In ViewDiDLoad")
         // Do any additional setup after loading the view.
     }
 
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if segue.identifier == "mySegue" {
+        
+        let nextVC = segue.destination as! SecondVC
+            
+        let button = sender as! UIButton
+        
+        if button.tag == 1 {
+            print(" In button 1")
+                nextVC.fromtext = "This is button one"
+            }else
+            if button.tag == 2 {
+                print(" In button 1")
+                nextVC.fromtext = "This is button two"
+            }}
+    }
 }
 
